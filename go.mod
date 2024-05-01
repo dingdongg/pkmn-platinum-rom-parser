fbe333d0ee752b95112a66f8dfdd0e97429458c3
dingdongg <cs.dg.kim@gmail.com>
2024-04-30 21:44:50 -0600

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module dingdongg/pkmn-platinum-rom-parser
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+const MONEY_OFFSET = 0x7c
+const MONEY_SIZE = 4
+
+func main() {
+	fmt.Println("HELLO WORLD")
+}
