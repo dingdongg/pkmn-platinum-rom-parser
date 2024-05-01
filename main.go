955eb30e757f7fa8cc5a621def547737e848d7da
dingdongg <cs.dg.kim@gmail.com>
2024-04-30 22:37:46 -0600

    get familiar with io reader

--- main.go
+++ main.go
@@ -1,10 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+const CHUNK_SIZE = 1576
 
 const MONEY_OFFSET = 0x7c
 const MONEY_SIZE = 4
 
 func main() {
 	fmt.Println("HELLO WORLD")
+
+	buf := make([]byte, CHUNK_SIZE)
+
+	file, err := os.Open("./Plat savefile")
+	if err != nil {
+		log.Fatal("bruh")
+	}
+	defer file.Close()
+
+	io.ReadFull(file, buf)
+	
+	// get da money info (should be 133440 in decimal)
+	money := binary.LittleEndian.Uint32(buf[MONEY_OFFSET:MONEY_OFFSET + MONEY_SIZE])
+
+	fmt.Println(money)
 }
