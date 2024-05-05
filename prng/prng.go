c6c3c565c124eafec04391ff91702af30edeb6be
dingdongg <cs.dg.kim@gmail.com>
2024-05-04 18:36:35 -0600

    add encryption algorithm for battle stat info

--- prng/prng.go
+++ prng/prng.go
@@ -6,6 +6,23 @@ type PRNG struct {
 	PrevResult uint
 }
 
+type BattleStatPRNG struct {
+	PrevResult uint
+	Personality uint32
+}
+
+func InitBattleStatPRNG(personality uint32) BattleStatPRNG {
+	return BattleStatPRNG{uint(personality), personality}
+}
+
+func (bsprng *BattleStatPRNG) Next() uint16 {
+	result := 0x041C64E6D * bsprng.PrevResult + 0x06073
+	bsprng.PrevResult = result
+	result >>= 16
+	// return the upper 16 bits only for external use; internally, all bits should be held for future calls
+	return uint16(result & 0xFFFF) 
+}
+
 func Init(checksum uint16, personality uint32) PRNG {
 	return PRNG{checksum, personality, uint(checksum)}
 }
